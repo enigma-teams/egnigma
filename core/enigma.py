1ddf60d33687bd2b6e88bb39b21d831f004d16d6
kouahou <koko1306@student.iepscf-uccle.be>
2022-02-22 20:09:36 +0100

    =add generatrice fonction

--- core/enigma.py
+++ core/enigma.py
@@ -1,33 +1,35 @@
 import unicodedata
 
 
-def LectureMessage(filename, result):
+def lecture_message(filename, result):
     with open(filename) as f:
-        try:
-            while f:
-                line = f.next()
-                line = " ".join(line.split())
-                line = "".join(c for c in unicodedata.normalize("NFD", unicode(line, encoding="utf-8")) if
-                               unicodedata.category(c) != "Mn")
-                result.append(line.upper())
-        except StopIteration as s:
-            pass
+        g = generate(f)
+        result.append("".join(g))
 
 
-def SauvegardeMessage(file_name, message_to_send):
+def generate(it):
+    for line in it:
+        i = ("".join(
+            d for d in unicodedata.normalize("NFD", unicode("".join(line.split()), encoding="utf-8"))
+            if unicodedata.category(d) != "Mn"))
+        if i:
+            yield i.upper()
+
+
+def save_message(file_name, message_to_send):
     with open(file_name, mode="w") as f:
         f.write(message_to_send)
 
 
 def menu():
-    mesg = []
-    LectureMessage("message.txt", mesg)
+    message_to_sent = []
+    lecture_message("message.txt", message_to_sent)
 
-    message = " ".join([c for c in mesg])
+    message = message_to_sent[0]
 
     mes = "Nouveau message"
 
-    SauvegardeMessage("sent.txt", mes)
+    save_message("sent.txt", mes)
     print (message)
 
 
