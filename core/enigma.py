a14028758b1bd259082d991b4763dd10cbb2f99f
kouahou <koko1306@student.iepscf-uccle.be>
2022-02-22 23:14:00 +0100

    =add load of rotor and reflector settings

--- core/enigma.py
+++ core/enigma.py
@@ -1,10 +1,16 @@
 import unicodedata
 
+import json
 
-def lecture_message(filename, result):
-    with open(filename) as f:
-        g = generate(f)
-        result.append("".join(g))
+settings_file = "rotors.init"
+
+message_file = "message.txt"
+
+save_file = "sent.txt"
+
+# Dictionary with rotors and reflector
+rotor = {}
+reflector = {}
 
 
 def generate(it):
@@ -16,20 +22,43 @@ def generate(it):
             yield i.upper()
 
 
+def lecture_message(filename, result):
+    with open(filename) as f:
+        g = generate(f)
+        result.append("".join(g))
+
+
 def save_message(file_name, message_to_send):
     with open(file_name, mode="w") as f:
         f.write(message_to_send)
 
 
+def load_params():
+    with open(settings_file, 'r') as p:
+        init_rotor_and_reflector(**json.load(p))
+
+
+def init_rotor_and_reflector(rotors, reflecteurs):
+    """ Setting the states of rotors """
+    global rotor
+    global reflector
+    rotor = rotors
+    reflector = reflecteurs
+
+
 def menu():
     message_to_sent = []
-    lecture_message("message.txt", message_to_sent)
+    lecture_message(message_file, message_to_sent)
 
     message = message_to_sent[0]
 
+    load_params()
+
+    print (reflector)
+
     mes = "Nouveau message"
 
-    save_message("sent.txt", mes)
+    save_message(save_file, mes)
     print (message)
 
 
